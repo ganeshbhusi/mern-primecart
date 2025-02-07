1a318975d7d8401ee2b536aacc57c9f63a9d06f1
Ganesh <ganeshpaul999@gmail.com>
2025-02-07 12:12:34 +0530

    test deploy [deploy]

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const NavBar = () => {
           bgGradient={"linear(to-r, cyan.400, blue.500)"}
         >
           <Link to={"/"}>PrimeCart 🛒</Link>
-          <sub>Test</sub>
+          <sub>Test 1</sub>
         </Text>
         <HStack wordSpacing={2} alignItems={"center"}>
           <Link to="/createProduct">
